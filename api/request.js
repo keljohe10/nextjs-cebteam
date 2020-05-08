39dbdffe474af3081ba3920a970795aa784f99c8
kelvinjose10 <khernandez@condorlabs.io>
2020-05-08 00:37:09 -0500

    .env

--- api/request.js
+++ api/request.js
@@ -2,8 +2,7 @@ import 'isomorphic-fetch';
 
 export const getProvider = async () => {
   const myHeaders = new Headers({
-    Authorization:
-      'Bearer sZa6s-IomsnwlmWmMyrMzeq64hWK87zMT7bYcNKjVro78H6JFzNRM2cOinI2AGBbXPbr0b7-3huDWIR07NxF1t-O8LcS7W5kBE00Ib0J0EPcZeH02N12OrM2IgwGyfQnCrQ0Oz2UCfDcnZtg6uGdjgJP3LdHcV-Rh7hbN0vdb0arUV9qFm5Z82xagLdK6iVCCTTg_NA0p-YPpd2YZFemQW1HyJrPSnWTs8hAkvfqUNhyGnSwm83wa31myWUb97PbsX6eq_0keXR6P7vREw17sSn2GmfAibPi30FeXJTr_pT1aYd9sr93xvj9KyygQqs_Xkq-VDjmLVfZzb7dEC3IwfeqzYMVXQAKdCEEpB1aTEOH-k3YrTF9wJzgsO5bNTsN3Oc_FC83jYAxO0a5dqfd2Oswp6o58TInidnHoOGP5rtkBFU67-MmT0NWpLnoIOMzVXcSntmGfHHqMuoAouPfTpxC628',
+    Authorization: `Bearer ${process.env.TOKEN}`,
   });
   var miInit = {
     method: 'GET',
@@ -20,8 +19,7 @@ export const getProvider = async () => {
 
 export const getCourses = async () => {
   const myHeaders = new Headers({
-    Authorization:
-      'Bearer sZa6s-IomsnwlmWmMyrMzeq64hWK87zMT7bYcNKjVro78H6JFzNRM2cOinI2AGBbXPbr0b7-3huDWIR07NxF1t-O8LcS7W5kBE00Ib0J0EPcZeH02N12OrM2IgwGyfQnCrQ0Oz2UCfDcnZtg6uGdjgJP3LdHcV-Rh7hbN0vdb0arUV9qFm5Z82xagLdK6iVCCTTg_NA0p-YPpd2YZFemQW1HyJrPSnWTs8hAkvfqUNhyGnSwm83wa31myWUb97PbsX6eq_0keXR6P7vREw17sSn2GmfAibPi30FeXJTr_pT1aYd9sr93xvj9KyygQqs_Xkq-VDjmLVfZzb7dEC3IwfeqzYMVXQAKdCEEpB1aTEOH-k3YrTF9wJzgsO5bNTsN3Oc_FC83jYAxO0a5dqfd2Oswp6o58TInidnHoOGP5rtkBFU67-MmT0NWpLnoIOMzVXcSntmGfHHqMuoAouPfTpxC628',
+    Authorization: `Bearer ${process.env.TOKEN}`,
   });
   var miInit = {
     method: 'GET',
---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+  const env = {
+    TOKEN:
+      '53-eo9u9hd47QG9OcdVaviNsLJX-qLBSJHkj2C4Cmg2FFkAUkbAT4Br4p-lewzkFqMR6B0Y2GVZ2qilQtTRr_DaF02xRKdWnMRjSZ5OXc0uX0ahlInqSQhLiIckts_QtOd53raUumy1FHFTYFDIMG6ONdWcahhgmbrlErXv94JzZKn1Al0_7GXPURmkgRxxXIkdzHZBkVO4DjmOoWE25uFd3Wae-r5p_p2xAfx24f-dWFF6x2oIkZmAiHqqkAo9wE-4fqJwgQU5PpGdM_i1H5DC4JcRwnuuopUdksDzMS_0BM02geZx4cQyihLFQkDmYY7YR_PPJcb3xrPzNg7PC5GKYccnDRxUZluBvC5mKlrnjf-pbSeSs8iVw3-Nb-a-PDlhYnS_evKjskppZyOGtQGa0RXuwyy0jmpRtZhQ5sLaPr7T3J_CsOYxYedx7Wy9-Wqw1lKqjSkFxHbZQmeuCAZIuMWs',
+  };
+
+  // next.config.js object
+  return {
+    env,
+  };
+};
